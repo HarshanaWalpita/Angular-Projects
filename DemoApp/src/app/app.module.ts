829648c0f0c09203a64abce5df52d4c2a9e3109c
HarshanaWalpita <harshanawalpita@gmail.com>
2021-10-16 22:54:37 +0530

    demo app

---
+++ DemoApp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient, HttpHeaders} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { ManageUserComponent } from './components/manage-user/manage-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateUserComponent,
+    ManageUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
